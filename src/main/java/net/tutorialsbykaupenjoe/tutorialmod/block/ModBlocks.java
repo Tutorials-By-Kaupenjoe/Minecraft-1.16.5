a44e829c158b713dd20e1a2ff90d2ec534d2e5f5
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-07-19 15:26:44 +0200

    added stairs

--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.tutorialsbykaupenjoe.tutorialmod.block;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -35,6 +36,10 @@ public class ModBlocks {
             () -> new FirestoneBlock(AbstractBlock.Properties.create(Material.IRON)
                     .harvestLevel(2).setRequiresTool().harvestTool(ToolType.PICKAXE).hardnessAndResistance(6f)));
 
+
+    public static final RegistryObject<Block> AMETHYST_STAIRS = registerBlock("amethyst_stairs",
+            () -> new StairsBlock(() -> AMETHYST_BLOCK.get().getDefaultState(),
+                    AbstractBlock.Properties.create(Material.IRON).harvestLevel(3).harvestTool(ToolType.PICKAXE).setRequiresTool()));
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
