02d98ea07ed368dc146bd6b6d1368d6b5608b882
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-07-20 14:42:54 +0200

    added tools

---
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/ModItemTier.java
@@ -0,0 +1,60 @@
+package net.tutorialsbykaupenjoe.tutorialmod.item;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    AMETHYST(3, 150, 4f, 12f, 10,
+            () -> Ingredient.fromItems(ModItems.AMETHYST.get()));
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency,
+                float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyValue<>(repairMaterial);
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.getValue();
+    }
+}
--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/ModItems.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.tutorialsbykaupenjoe.tutorialmod.item;
 
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,6 +19,31 @@ public class ModItems {
 
     public static final RegistryObject<Item> FIRESTONE = ITEMS.register("firestone",
             () -> new Firestone(new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP).maxDamage(8)));
+
+
+    public static final RegistryObject<Item> AMETHYST_SWORD = ITEMS.register("amethyst_sword",
+            () -> new SwordItem(ModItemTier.AMETHYST, 2, 3f,
+                    new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> AMETHYST_PICKAXE = ITEMS.register("amethyst_pickaxe",
+            () -> new PickaxeItem(ModItemTier.AMETHYST, 0, -1f,
+                    new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> AMETHYST_SHOVEL = ITEMS.register("amethyst_shovel",
+            () -> new ShovelItem(ModItemTier.AMETHYST, 0, -1f,
+                    new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> AMETHYST_AXE = ITEMS.register("amethyst_axe",
+            () -> new AxeItem(ModItemTier.AMETHYST, 4, -6f,
+                    new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> AMETHYST_HOE = ITEMS.register("amethyst_hoe",
+            () -> new HoeItem(ModItemTier.AMETHYST, 0, 0f,
+                    new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+
+
+
 
 
     public static void register(IEventBus eventBus) {
